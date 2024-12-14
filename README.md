1a3531b6208c324f35d498408807c976fd0bda91
Pietro Nuti <pietro.nuti93@gmail.com>
2024-12-14 17:29:35 +0100

    prettify

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ft_linear_regression
-A python project that implements linear regression with gradient descent from scratch 
+A python project that implements linear regression with gradient descent from scratch
+
+## Introduction
+The project is based on a [42](https://42berlin.de/) subject from the advanced curriculum. The goal is to make a linear regression model based on the gradient descent method that tries to predict the price of a car based on its mileage. It includes two parts: `train.py` and `predict.py`.  
+1. `train.py` reads into `data/data.csv` and trains the linear regression model. When the training has been completed, the program writes the calculated coefficients in `model.json`.
+2. `predict.py` accepts an integer as argument ( the mileage of a car ), reads `model.json` and uses the coefficients to predict the price for that car. 
+
+## Features
+The `train.py` program creates an animation with Matplotlib to show how the model "learns" over the iterations. The output is shown below:
+
+![animation](gd.gif)
+
+## Issues
+The subject requires to use the following formulas:
+$$ \theta _0 = learningRate * \frac{1}{m} \sum_{i=0}^{m - 1} (estimatedPrice(mileage[i]) - price[i])  $$
+$$ \theta _1 = learningRate * \frac{1}{m} \sum_{i=0}^{m - 1} (estimatedPrice(mileage[i]) - price[i]) * mileage[i] $$
+
+Although this does not change substantially the formulas that can be obtained via partial derivatives of the mean square error formula ( the one that should be used for gradient descent ), I personally found it rather confusing considering that no reference was given to what motivated the use of this simplified formula. For any other student encountering this issue (or for anyone wondering why I have used this specific formula) here is the information I could gather:
+* In this simplified formula the sign of the fraction and its nominator are changed, which should not radically change the final result because
+    * Changing the sign changes the "direction" in which the lowest point of the error curve is searched
+    * Changing the nominator simply impacts the learning rate, doubling it
+* In the equation of a line $ y = mx + b $
+    * $ \theta _0 $ corresponds to $ b $
+    * $ \theta _1 $ corresponds to $ m $
+
+
+## Method
+The model is trained on a dataset scaled with the min-max scaling method, and the coefficients in the output are re-scaled to the original dimensions by using the opposite process.  
+The model defaults to a learning rate of 0.05 and iterates over 1000 epochs. The optimal number of epochs actually falls a bit over one half of the default, but mostly due to aestetich reasons I extended it to 1000 (I wanted a longer MSE curve to show that the curve actually does not keep decreasing after reaching 600 epochs)
--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 # %%
 import sys
 import json
-from enum import Enum
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
@@ -14,15 +13,14 @@ mse: list[float] = []
 fig_data: np.ndarray
 line, = ax1.plot([], [], color="red")
 loss_line, = ax2.plot([], [], color="red")
-min_norm_data: float
-max_norm_data: float
-min_x: float
-max_x: float
-min_y: float
-max_y: float
+min_plot_data: float
+max_plot_data: float
 
 
 class bcolors:
+    """
+    Provides a list of colors that can be used in STDOUT
+    """
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
@@ -35,21 +33,51 @@ class bcolors:
 
 
 def animate(i):
+    """
+    Provides the logic to animate the graphs
+
+    :param i: iteration number
+    :type i: int
+    :returns: animated elements to be plotted
+    """
     line.set_data(
-        [min_norm_data,
-         max_norm_data],
-        [theta[i][0] * min_norm_data + theta[i][1],
-         theta[i][0] * max_norm_data + theta[i][1]])
-    loss_line.set_data(range(i), mse[:i])
+        [min_plot_data,
+         max_plot_data],
+        [theta[i][0] * min_plot_data + theta[i][1],
+         theta[i][0] * max_plot_data + theta[i][1]])
+    loss_line.set_data(range(0, i * 10, 10), mse[:i])
     return line, loss_line
 
 
-class Errors(Enum):
+def plot_data(data: np.ndarray, headers: list[str], epochs: int):
     """
-    Enum class that inherits from :class:`Enum`
+    Creates and saves the animated plots
+
+    :param data: 2D tensor used for the training of the model
+    :type data: np.ndarray
+    :param headers: the headers of the dataset
+    :type headers: list[str]
     """
-    NO_ERRORS = 0
-    INVALID_ARGUMENT = 1
+    global min_plot_data
+    global max_plot_data
+    min_plot_data = np.min(data)
+    max_plot_data = np.max(data)
+    ax1.set_box_aspect(1)
+    ax1.set_xlim([-0.1, 1.1])
+    ax1.set_ylim([-0.1, 1.1])
+    ax1.set_xlabel(headers[0])
+    ax1.set_ylabel(headers[1])
+    ax2.set_box_aspect(1)
+    ax2.set_xlim(0, epochs)
+    ax2.set_ylim(0, max(mse) * 1.1)
+    ax2.set_xlabel("Epoch")
+    ax2.set_ylabel("Mean Squared Error (MSE)")
+    plt.subplots_adjust(wspace=0.5, hspace=0.2)
+    ax1.scatter(data[0], data[1])
+    anim = animation.FuncAnimation(fig=fig, func=animate, frames=len(mse))
+    anim.save("gd.gif", fps=30)
+    plt.tight_layout()
+    plt.show()
 
 
 def log_error(message: str) -> None:
@@ -83,11 +111,19 @@ def read_csv(path: str) -> tuple[np.ndarray, list[str]]:
 
 
 def write_to_json(theta0: float, theta1: float) -> None:
+    """
+    Writes the two coefficients to a json file named "model.json"
+
+    :param theta0: the x intercept of the regression line
+    :type theta0: float
+    :param theta1: the slope of the regression line
+    :type theta1: float
+    """
     with open("model.json", "w") as f:
         json.dump({"theta0": theta0, "theta1": theta1}, f)
 
 
-def min_max_normalize(data: np.ndarray) -> np.ndarray:
+def min_max_scale(data: np.ndarray) -> np.ndarray:
     """
     Normalize 2D tensor with feature scaling method
 
@@ -107,7 +143,26 @@ def min_max_normalize(data: np.ndarray) -> np.ndarray:
 
 def scale_coefficients(
         theta0: float,
-        theta1: float) -> tuple[float, float]:
+        theta1: float,
+        data: np.ndarray) -> tuple[float, float]:
+    """
+    Scales the coefficient so that they can be used to calculate the predicted
+    dependent variable. This should be used only if the coefficients were
+    calculated on a dataset where min-max scaling was used
+
+    :param theta0: the x intercept of the regression line
+    :type theta0: float
+    :param theta1: the slope of the regression line
+    :type theta1: float
+    :param data: the original data (before scaling)
+    :type data: np.ndarray
+    :return: the scaled coefficients
+    :rtype: tuple[float, float]
+    """
+    min_x: float = np.min(data[0])
+    max_x: float = np.max(data[0])
+    min_y: float = np.min(data[1])
+    max_y: float = np.max(data[1])
     th1: float = theta1 * (max_y - min_y) / (max_x - min_x)
     th0: float = min_y + theta0 * (max_y - min_y) - th1 * min_x
     return th0, th1
@@ -115,44 +170,67 @@ def scale_coefficients(
 
 def linear_regression_train(
         data: np.ndarray[np.ndarray[float]],
+        headers: list[str],
         learning_rate: float = 0.02,
-        epochs: int = 1000) -> None:
+        epochs: int = 1000,
+        should_scale_data: bool = True,
+        should_plot_data: bool = True) -> None:
     """
     Trains the model with the data provided to this function
 
     :param data: 2D tensor. The data is expected to be divided in two
                 sub-arrays
     :type data: np.ndarray
+    :param headers: the headers of the training dataset. Must be provided if
+                the data is going to be plotted
+    :type headers: list[str]
     :param alpha: the learning rate of the model, defaults to 0.01
     :type alpha: float
     :param epochs: the epochs (number of iterations) used by the model to
                 get fine-tuned, defaults to 1000
     :type epochs: int
+    :param should_scale_data: defines if min max scaling will be used on the
+                dataset, defaults to True
+    :type should_scale_data: bool
+    :param should_plot_data: defines if the data will be plotted,
+                defaults to True
+    :type should_plot_data: bool
     """
+    if len(data) != 2:
+        raise Exception("The function expects a 2D tensor")
+    if should_plot_data and len(headers) != 2:
+        raise Exception("Headers should be provided in order to create plot")
+    if should_scale_data:
+        original_data = data
+        data = min_max_scale(data)
     (x, y) = data
     m, b = .0, .0
     n = len(x)
-    div = -2 / n
     for i in range(epochs):
         y_pred = m * x + b
-        diff_vectors = y - y_pred
-        dm = div * np.sum(x * diff_vectors)
-        db = div * np.sum(diff_vectors)
+        dm = (-2 / n) * np.sum(x * (y - y_pred))
+        db = (-2 / n) * np.sum((y - y_pred))
 
         m -= learning_rate * dm
         b -= learning_rate * db
 
-        if i % 10 == 0:
+        if should_plot_data and i % 10 == 0:
             mse.append((1 / n) * np.sum((y - y_pred) ** 2))
             theta.append((m, b))
-    th0, th1 = scale_coefficients(b, m)
-    write_to_json(th0, th1)
+    if should_scale_data:
+        b, m = scale_coefficients(b, m, original_data)
+    write_to_json(b, m)
+    if should_plot_data:
+        plot_data(data, headers, epochs)
 
 
 def linear_regression_train_subject(
         data: np.ndarray[np.ndarray[float]],
+        headers: list[str],
         learning_rate: float = 0.05,
-        epochs: int = 1000) -> None:
+        epochs: int = 1000,
+        should_scale_data: bool = True,
+        should_plot_data: bool = True) -> None:
     """
     Trains the model with the data provided to this function. This version of
     the function uses the formulas presented in the subject instead of the ones
@@ -161,72 +239,58 @@ def linear_regression_train_subject(
     :param data: 2D tensor. The data is expected to be divided in two
                 sub-arrays
     :type data: np.ndarray
-    :param alpha: the learning rate of the model, defaults to 0.01
-    :type alpha: float
+    :param headers: the headers of the training dataset. Must be provided if
+                the data is going to be plotted
+    :type headers: list[str]
+    :param learning_rate: the learning rate of the model, defaults to 0.01
+    :type learning_rate: float
     :param epochs: the epochs (number of iterations) used by the model to
                 get fine-tuned, defaults to 1000
     :type epochs: int
+    :param should_scale_data: defines if min max scaling will be used on the
+                dataset, defaults to True
+    :type should_scale_data: bool
+    :param should_plot_data: defines if the data will be plotted,
+                defaults to True
+    :type should_plot_data: bool
     """
+    if len(data) != 2:
+        raise IndexError("The function expects a 2D tensor")
+    if should_plot_data and len(headers) != 2:
+        raise IndexError("Headers should be provided in order to create plot")
+    if should_scale_data:
+        original_data = data
+        data = min_max_scale(data)
     (x, y) = data
     theta0, theta1 = .0, .0
     n = len(x)
-    div = 1 / n
     for i in range(epochs):
         y_pred = theta0 + theta1 * x
-        diff_vectors = y_pred - y
-        theta0 -= learning_rate * div * np.sum(diff_vectors)
-        theta1 -= learning_rate * div * np.sum(diff_vectors * x)
+        theta0 -= learning_rate * (1 / n) * np.sum((y_pred - y))
+        theta1 -= learning_rate * (1 / n) * np.sum((y_pred - y) * x)
 
-        if i % 10 == 0:
+        if should_plot_data and i % 10 == 0:
             mse.append((1 / n) * np.sum((y - y_pred) ** 2))
             theta.append((theta1, theta0))
-    th0, th1 = scale_coefficients(theta0, theta1)
-    write_to_json(th0, th1)
+    if should_scale_data:
+        theta0, theta1 = scale_coefficients(theta0, theta1, original_data)
+    write_to_json(theta0, theta1)
+    if should_plot_data:
+        plot_data(data, headers, epochs)
 
 
 def main():
     """
-    The program accepts 1 argument which defines
-    the path to the training data
+    The program calculates the coefficients used in linear regression with
+    gradient descent and exports them in `model.json`
     """
-    """ if (len(sys.argv) != 2
-            or not os.path.isfile(sys.argv[1])
-            or not sys.argv[1].endswith(".csv")):
-        log_error("Provide a valid path to a .csv file")
-        exit(Errors.INVALID_ARGUMENT.value) """
-    (data, headers) = read_csv("./data/data.csv")
-    global min_x
-    global max_x
-    global min_y
-    global max_y
-    min_x = np.min(data[0])
-    max_x = np.max(data[0])
-    min_y = np.min(data[1])
-    max_y = np.max(data[1])
-    norm_data = min_max_normalize(data)
-    global fig_data
-    global min_norm_data
-    global max_norm_data
-    fig_data = norm_data
-    min_norm_data = min(fig_data[0])
-    max_norm_data = max(fig_data[0])
-    linear_regression_train_subject(norm_data)
-    ax1.set_box_aspect(1)
-    ax1.set_xlim([-0.1, 1.1])
-    ax1.set_ylim([-0.1, 1.1])
-    ax1.set_xlabel(headers[0])
-    ax1.set_ylabel(headers[1])
-    ax2.set_box_aspect(1)
-    ax2.set_xlim(0, 100)
-    ax2.set_ylim(0, max(mse) * 1.1)
-    ax2.set_xlabel("Epoch")
-    ax2.set_ylabel("Mean Squared Error (MSE)")
-    plt.subplots_adjust(wspace=0.5, hspace=0.2)
-    ax1.scatter(fig_data[0], fig_data[1])
-    anim = animation.FuncAnimation(fig=fig, func=animate, frames=len(mse))
-    anim.save("gd.gif", fps=30)
-    plt.tight_layout()
-    plt.show()
+    try:
+        (data, headers) = read_csv("./data/data.csv")
+        linear_regression_train_subject(data, headers)
+    except FileNotFoundError:
+        log_error("data file does not exist")
+    except IndexError as index_error:
+        log_error(index_error.args[0])
 
 
 if __name__ == "__main__":
